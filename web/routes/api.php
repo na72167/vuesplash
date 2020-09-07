9b384c9aa50223186fab959e9e36f5572fc81e5f
yh72167 <yh.19972372@gmail.com>
2020-09-07 12:00:10 +0900

    [add]最初のコミット

---
+++ web/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
